a66f0b41005b599bbc5b61f05d9f9376928596eb
Andres <andrescasa.dev@gmail.com>
2022-10-04 10:35:58 -0500

    flex: online users responsive

--- Flex/onlineUsers/components/userComponent.js
+++ Flex/onlineUsers/components/userComponent.js
@@ -2,7 +2,7 @@ export default function userComponent(user){
   const {name} = user
   return `
   <div class="flex p-1 border-b-[.001em] border-slate-500 items-center ">
-    <div class="relative w-[2em] h-[2em] rounded-full bg-gray-400">
+    <div class="shrink-0 relative w-[2em] h-[2em] rounded-full bg-gray-400">
       <div class="absolute w-[1em] h-[1em] bottom-[-3px] right-[-5px] rounded-full bg-green-400 border-4 border-slate-600"></div>
     </div>
     <div class="flex items-center gap-4 p-4 ">
